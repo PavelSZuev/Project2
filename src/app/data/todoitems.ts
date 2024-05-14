ebb4fa62afd60bf57cf94c73b1005ac9b67a82c7
name <email>
2024-05-14 20:44:11 +0300

    first commit

---
+++ src/app/data/todoitems.ts
@@ -0,0 +1,5 @@
+export const todoItems = [
+    { id: 1, name: 'Задача 1', isCompete: false },
+    { id: 1, name: 'Задача 1', isCompete: false },
+    { id: 1, name: 'Задача 1', isCompete: false },
+];
