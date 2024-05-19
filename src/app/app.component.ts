baed5fbfe555ca35b4cc363326648d7b7238fc15
name <email>
2024-05-19 20:55:15 +0300

    19/05/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { todoItems } from './data/todoitems';
 import { NgFor } from "@angular/common"
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,19 @@ import { NgFor } from "@angular/common"
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild("pole") todoInputValue: ElementRef<HTMLInputElement> = null!;
   title = 'ToDo';
   items = todoItems;
+  addText(text:string):void{
+    if (text.trim() !== "") {
+    const date = new Date();
+    const ms = date.getTime();
+    this.items.push({ id: ms, name: text, isCompete: false })
+    this.todoInputValue.nativeElement.value = "";
+    }
+  }  
+  delText(itemToDelete:number){
+    this.items = this.items.filter((item) => item.id !== itemToDelete)
+  }
 }
+
--- src/app/data/todoitems.ts
+++ src/app/data/todoitems.ts
@@ -1,5 +1,5 @@
 export const todoItems = [
     { id: 1, name: 'Задача 1', isCompete: false },
-    { id: 1, name: 'Задача 1', isCompete: false },
-    { id: 1, name: 'Задача 1', isCompete: false },
+    { id: 2, name: 'Задача 2', isCompete: false },
+    { id: 3, name: 'Задача 3', isCompete: false },
 ];
